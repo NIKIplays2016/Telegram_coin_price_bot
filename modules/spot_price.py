76ebd57f89ba684e688293147b805fe8381f3ff7
Mikita <48063681+NIKIplays2016@users.noreply.github.com>
2024-06-01 00:12:14 +0300

    Add files via upload

--- modules/spot_price.py
+++ modules/spot_price.py
@@ -1,8 +1,63 @@
 import okx.MarketData as MarketData
+from threading import Thread
+from functools import partial
 
-def get_price():
+def loop1(market, token_base, local_tokens):
+    tokens = token_base["tokens"]
+    for i in range(0, len(tokens), 3):
+        local_tokens[tokens[i]] = format(float(market.get_orderbook(
+            instId=f"{tokens[i]}-{token_base['fiat_token']}"
+        )["data"][0]["asks"][0][0]), '.10f').rstrip('0').rstrip('.')
+
+    return local_tokens
+
+
+def loop2(market, token_base,local_tokens):
+    tokens = token_base["tokens"]
+    for i in range(1, len(tokens), 3):
+        local_tokens[tokens[i]] = format(float(market.get_orderbook(
+            instId=f"{tokens[i]}-{token_base['fiat_token']}"
+        )["data"][0]["asks"][0][0]), '.10f').rstrip('0').rstrip('.')
+
+    return local_tokens
+
+
+def loop3(market, token_base, local_tokens):
+    tokens = token_base["tokens"]
+    for i in range(2, len(tokens), 3):
+        local_tokens[tokens[i]] = format(float(market.get_orderbook(
+            instId=f"{tokens[i]}-{token_base['fiat_token']}"
+        )["data"][0]["asks"][0][0]), '.10f').rstrip('0').rstrip('.')
+
+
+
+def get_price(token_base):
+    tokens = {}
+
+    local_tokens1 = {}
+    local_tokens2 = {}
+    local_tokens3 = {}
+
     marketDataAPI = MarketData.MarketAPI(flag="0")
-    result = marketDataAPI.get_orderbook(
-        instId="NOT-USDT"
-    )
-    return {"buy": result["data"][0]["asks"][0][0], "sell": result["data"][0]["bids"][0][0]}
+
+    partial_func1 = partial(loop1, marketDataAPI, token_base, local_tokens1)
+    partial_func2 = partial(loop2, marketDataAPI, token_base, local_tokens2)
+    partial_func3 = partial(loop3, marketDataAPI, token_base, local_tokens3)
+
+    tokens1 = Thread(target=partial_func1)
+    tokens2 = Thread(target=partial_func2)
+    tokens3 = Thread(target=partial_func3)
+
+    tokens1.start()
+    tokens2.start()
+    tokens3.start()
+
+    tokens1.join()
+    tokens2.join()
+    tokens3.join()
+
+    tokens = local_tokens1 | local_tokens2 | local_tokens3
+
+    return tokens
+
+
--- modules/tg_bot.py
+++ modules/tg_bot.py
@@ -28,11 +28,11 @@ def registrate(message, base):
     id = message.chat.id
     base["id"].append(id)
     base["root"].append(False)
-    base["Question"]["spam"].append(False)
-    base["Question"]["warning"].append(False)
-    base["limit"].append([1,1])
     base["none_reg"].remove(id)
-    base["bool_actions"].append([False, False, False, False, False])
+    base["bool_actions"].append([False, False, False, False, False, False, False])
+    base["spam"]["token"].append([])
+    base["warning"]["token"].append([])
+    base["warning"]["limit"].append([])
     if id > 0:
         base["name"].append(message.from_user.user_name)
     elif id < 0:
@@ -44,6 +44,10 @@ def registrate(message, base):
 
 
 def bot_main():
+    with open("base\static.json", "r") as file:
+        token_base = json.load(file)
+
+
     global bot
 
     def check_registrate(id, base) -> bool:
@@ -70,6 +74,20 @@ def bot_main():
             return False
 
 
+    def reset_actions():
+        return [False, False, False, False, False, False, False]
+
+    @bot.message_handler(commands=['start'])
+    def handle_start(message):
+        id = message.chat.id
+        base = update_date()
+        if id not in base["id"] and not id in base["none_reg"]:
+            bot.reply_to(message, text="Введите пароль")
+            base["none_reg"].append(id)
+            save(base)
+
+
+
     @bot.message_handler(commands=['command1'])
     def handle_start(message):
         id = message.chat.id
@@ -83,12 +101,14 @@ def bot_main():
             bot.reply_to(message, f"Вы были заблокированы. \nПоддержка: @picard_off")
             return 0
 
-        if base["Question"]["spam"][index]:
-            base["Question"]["spam"][index] = False
-            bot.reply_to(message, f"Вы отписались от рассылки \nTrue -> False")
-        else:
-            base["Question"]["spam"][index] = True
-            bot.reply_to(message, f"Вы подписались на рассылку \nFalse -> True")
+        base["bool_actions"][index] = reset_actions()
+
+        keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+        for i in token_base["tokens"]:
+            keyboard1.row(i)
+
+        base["bool_actions"][index][6] = True
+        bot.reply_to(message, "Введите короткое название монеты", reply_markup=keyboard1)
         save(base)
 
     @bot.message_handler(commands=['command2'])
@@ -104,19 +124,14 @@ def bot_main():
             bot.reply_to(message, f"Вы были заблокированы. \nПоддержка: @picard_off")
             return 0
 
-        if base["Question"]["warning"][index]:
-            base["Question"]["warning"][index] = False
-            bot.reply_to(message, f"Вы отписались от предупреждений \nTrue -> False")
-        else:
-            base["Question"]["warning"][index] = True
-            bot.reply_to(message, f"Вы подписались на предупреждения \nFalse -> True")
+        base["bool_actions"][index] = reset_actions()
 
-            base["bool_actions"][index][0] = True
-            bot.reply_to(message, "Введите через пробел пределы. \n\nПример: \n0.0043 0.006")
+        base["bool_actions"][index][0] = True
+        bot.reply_to(message, "Введите через пробел короткое называние монеты и пределы. \n\nПример: \TON 6.43 7.1")
 
         save(base)
 
-    @bot.message_handler(commands=['command3'])
+    @bot.message_handler(commands=['command3'])  # Удаление монеты из warning
     def handle_start(message):
         id = message.chat.id
         base = update_date()
@@ -129,32 +144,69 @@ def bot_main():
             bot.reply_to(message, f"Вы были заблокированы. \nПоддержка: @picard_off")
             return 0
 
-        if base["Question"]["warning"][index]:
-            base["bool_actions"][index][0] = True
-            bot.reply_to(message, "Введите через пробел пределы. \n\nПример: \n 0.0043 0.006")
-        else:
-            bot.reply_to(message, r"Вам нужно включить рассылку предупреждений \command2")
+        base["bool_actions"][index] = reset_actions()
+
+        base["bool_actions"][index][5] = True
+        bot.reply_to(message, "Введите короткое название монеты")
+
         save(base)
+
+
+
+
+
+
+
+
+
+    @bot.message_handler(commands=['about_me'])
+    def handle_start(message):
+        id = message.chat.id
+        base = update_date()
+        if not check_registrate(id, base):
+            bot.reply_to(message, text="Вы не авторизованны. \nВведите пароль")
+            return 0
+        index = base["id"].index(id)
+
+        if id in base["black_list"]:
+            bot.reply_to(message, f"Вы были заблокированы. \nПоддержка: @picard_off")
+            return 0
+
+        sms = f"id: {id}"
+
+        sms += "\n\nУведомления о цене:"
+        sms += f"\n{base['spam']['token'][index]}"
+
+        sms += "\n\nУведомления при выходе монеты из ценового диапазона:\n"
+        for i in range(len(base['warning']['token'][index])):
+            sms += f"\nМонета: {base['warning']['token'][index][i]} \nЦеновой диапазон: с {base['warning']['limit'][index][i][0]} до {base['warning']['limit'][index][i][1]}\n"
+
+        bot.reply_to(message, sms)
+
+
+
+###########################################################s
+    """определить и вызвать функцию, понимать механизм перегрузки"""
+
+
 
     @bot.message_handler(commands=['command0', "help", "info"])
     def handle_start(message):
         bot.reply_to(
             message,
             text=f""" 
-'NOT price' - Это Telegram бот позволяющий следить за ценой на Not coin.
-v0.23 Beta
+'Coin price' - Это Telegram бот позволяющий следить за ценой разных криптовалют.
+v0.50 Beta
 
 Информация о цене берется с okx.com
 
 Команды:
- /command1 - При вводе этой команды бот будет присылать каждую минуту цену NOT, чтобы отписаться нужно повторно ввести команду.
+ /command1 - При вводе этой команды бот будет присылать каждую минуту цену на введенную вами монету, чтобы отписаться нужно повторно ввести команду.
 
- /command2 - При вводе этой команды бот предлагает вам ввести верхний и нижний "предел" цены. 
+ /command2 - При вводе этой команды бот предлагает вам ввести название монеты, верхний и нижний "предел" цены. 
 При переходе нижнего или верхнего предела цены, бот уведомит вас об этом. 
-Чтобы отписаться нужно повторно ввести команду. 
-В случае ошибки ввода нужно ввести /command3
 
- /command3 - Позволяет заново задать нижний и верхний "предел цены"
+ /command3 - При вводе этой комманды нужно ввести название монеты, и вам больше не будут приходить уведомления о выходе цены этой манеты из ценового диапазона который вы указали ранее (/command2)  
 
 
 @picard_off
@@ -201,9 +253,11 @@ okx API
             return 0
 
         index = base["id"].index(id)
-        if not (id == base["main_admin"] or message.from_user.id == base["main_admin"]) :
+        if not id == base["main_admin"]:
             bot.reply_to(message, "У вас нет прав для этой команды. Это может делать только главный админ")
             return 0
+
+        base["bool_actions"][index] = reset_actions()
 
         base["bool_actions"][index][2] = True
         bot.reply_to(message, "Введите id пользователя")
@@ -219,9 +273,12 @@ okx API
             return 0
 
         index = base["id"].index(id)
-        if not (id == base["main_admin"] or message.from_user.id == base["main_admin"]) :
+        if not id == base["main_admin"]:
             bot.reply_to(message, "У вас нет прав для этой команды. Это может делать только главный админ")
             return 0
+
+        base["bool_actions"][index] = reset_actions()
+
         base["bool_actions"][index][3] = True
         bot.reply_to(message, "Введите id пользователя")
         save(base)
@@ -239,6 +296,9 @@ okx API
         if not id == base["main_admin"]:
             bot.reply_to(message, "У вас нет прав для этой команды. Это может делать только главный админ")
             return 0
+
+        base["bool_actions"][index] = reset_actions()
+
         base["bool_actions"][index][4] = True
         bot.reply_to(message, "Введите id пользователя")
         save(base)
@@ -257,6 +317,8 @@ okx API
         if not check_admin(message, base):
             bot.reply_to(message, "У вас нет прав для этой команды")
             return 0
+
+        base["bool_actions"][index] = reset_actions()
 
         base["bool_actions"][index][1] =True
         bot.reply_to(message, "Введите id и действие(0 - убрать из чс, 1 - добавить в чс) \n\nПример: \n1913991 1")
@@ -294,6 +356,8 @@ okx API
 
 
 
+
+
     @bot.message_handler(content_types=['text', 'document', 'audio', 'photo', 'video'])
     def get_text_messages(message):
         base = update_date()
@@ -312,7 +376,7 @@ okx API
 
 
         if id not in base["id"] and not id in base["none_reg"]:
-            bot.reply_to(message, text="Write password")
+            bot.reply_to(message, text="Введите пароль")
             base["none_reg"].append(id)
             save(base)
             return 0
@@ -332,15 +396,36 @@ okx API
 
         if base["bool_actions"][index][0]:
             try:
-                lim = message.text.split(" ")
-                lim[0], lim[1] = float(lim[0]), float(lim[1])
-                base["limit"][index][0] = lim[0]
-                base["limit"][index][1] = lim[1]
-                base["bool_actions"][index][0] = False
-                save(base)
+                command = message.text.split(" ")
+                token, command[1], command[2] = command[0].upper(), float(command[1]), float(command[2])
             except:
-                bot.reply_to(message, "❌Неверный ввод❌ \nПопробуйте заново нажав на команду /command3")
+                bot.reply_to(message, "❌Неверный ввод❌ \nПопробуйте заново нажав на команду /command2")
                 base["bool_actions"][index][0] = False
+                return 0
+
+            if not token in token_base["tokens"]:
+                bot.reply_to(message, "Такой монеты к нет в списке")
+                base["bool_actions"][index][0] = False
+                return 0
+
+            """if token in base["warning"]["token"][index]:
+                token_index = base["warning"]["token"][index].index(token)
+                base["warning"]["token"][index].remove(token)
+                base["warning"]["limit"][index].pop(token_index)
+                bot.reply_to(message, f"Теперь вам не будет приходить уведомление о {token}")"""
+
+            if token in base["warning"]["token"][index]:
+                token_index = base["warning"]["token"][index].index(token)
+                base["warning"]["limit"][index][token_index] = [command[1], command[2]]
+                bot.reply_to(message, f"Теперь вам будет приходить уведомление когда цена {token} \nПовысится до {command[2]}$ \nУпадет до {command[1]}$")
+            else:
+                base["warning"]["token"][index].append(token)
+                base["warning"]["limit"][index].append([command[1], command[2]])
+                bot.reply_to(message, f"Теперь вам будет приходить уведомление когда цена {token} \nПовысится до {command[2]}$ \nУпадет до {command[1]}$")
+
+            base["bool_actions"][index][0] = False
+            save(base)
+
 
         elif base["bool_actions"][index][1]:
             command = message.text.split(" ")
@@ -415,14 +500,45 @@ okx API
                 save(base)
                 return 0
 
-            bot.reply_to(message, f"Главный админ изменен с {base["main_admin"]} на {nid}")
+            bot.reply_to(message, f"Главный админ изменен с {base['main_admin']} на {nid}")
             base["main_admin"] = nid
             base["bool_actions"][index][4] = False
             save(base)
 
-        else:
-            pass  # bot.reply_to(message, "Действие не выбрано")
 
-    bot.polling(none_stop=True, interval=0)
+
+
+
+        elif base["bool_actions"][index][5]: #Удаление токена с warning
+            token = message.text.upper()
+
+
+            if token in base["warning"]["token"][index]:
+                token_index = base["warning"]["token"][index].index(token)
+                base["warning"]["token"][index].remove(token)
+                base["warning"]["limit"][index].pop(token_index)
+                bot.reply_to(message, f"Теперь вам не будет приходить уведомление о {token}")
+            else:
+                bot.reply_to(message, f"У вас не стоит 'колокольчик' на {token}")
+            base["bool_actions"][index][5] = False
+
+            save(base)
+
+        elif base["bool_actions"][index][6]:
+            sms = message.text.upper()
+            if not sms in token_base["tokens"]:
+                bot.reply_to(message, "Такой монеты к нет в списке")
+                base["bool_actions"][index][6] = False
+                return 0
+
+            if sms in base["spam"]["token"][index]:
+                base["spam"]["token"][index].remove(sms)
+                bot.reply_to(message, f"Теперь вы не будете получать рассылку цены на {sms}")
+            else:
+                base["spam"]["token"][index].append(sms)
+                bot.reply_to(message, f"Теперь вы будете получать рассылку цены на {sms}")
+            base["bool_actions"][index][6] = False
+            save(base)
+
 
     bot.polling(none_stop=True, interval=0)
